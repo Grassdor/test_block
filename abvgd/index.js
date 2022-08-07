9c824760f450cc25a38d02790a35ab3c867d372f
Grassdor <rrr345@mail.ru>
2022-08-07 11:31:28 +0300

    repack

--- abvgd/index.js
+++ abvgd/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const connectDB = require('./config/db')
 
 const app = express();
+
+connectDB();
 
 app.get('/hui', (req, res) => res.send('Hello world!'));
 
 const port = process.env.PORT || 8082;
 
 app.listen(port, () =>console.log(`Serverrunning on port ${port}`));
-
-
